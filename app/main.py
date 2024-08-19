d4a91ad7489bd57a25ae31ba0cb6fd49ffaa6022
Duane Faria <dfaria@MWKS0057550.corp.halliburton.com>
2024-08-18 23:00:58 -0300

    feat: check if a string has digits

--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ import sys
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    if pattern == '\\d':
+        for character in input_line:
+            if character.isdigit():
+                return True
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
